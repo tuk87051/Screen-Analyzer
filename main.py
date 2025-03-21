b6cdcedcf3c482d7f2d5ff893dfaf438aacadf28
tuk87051 <chungmatthew01@gmail.com>
2025-03-20 20:27:46 -0400

    changed pyautogui to mss

--- main.py
+++ main.py
@@ -2,26 +2,29 @@ import cv2 as cv
 import numpy as np
 import pyautogui
 from time import time
+import mss
 
-cap = cv.VideoCapture(0)
-if not cap.isOpened():
-    print("Cannot open camera")
-    exit()
+w, h = pyautogui.size()
+print("Screen resolution size: " + str(w) + str(h))
 
 loop_time = time()
-while True:
 
-    screenshot = pyautogui.screenshot()
-    screenshot = np.array(screenshot)
-    screenshot = cv.cvtColor(screenshot, cv.COLOR_RGB2BGR)
+with mss.mss() as sct:
 
-    # Display the resulting frame
-    cv.imshow('frame', screenshot)
+    monitor = {'left': 0, 'top': 0, 'width': w, 'height': h}
 
-    print('FPS {}'.format(1 / (time() - loop_time)))
-    loop_time = time()
+    while True:
 
-    if cv.waitKey(1) == ord('q'):
-        cv.destroyAllWindows()
-        break
+        screenshot = sct.grab(monitor)
+        screenshot = np.array(screenshot)
+
+        # Display the resulting frame
+        cv.imshow('frame', screenshot)
+
+        print('FPS {}'.format(1 / (time() - loop_time)))
+        loop_time = time()
+
+        if cv.waitKey(1) == ord('q'):
+            cv.destroyAllWindows()
+            break
 
